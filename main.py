d2bece761d1381ad00c981cc478fd94b5ce9fe23
Maria Lipina <m.o.lip714@gmail.com>
2023-02-15 11:39:17 +0300

    csv считывается ближе к тому как мне это нужно

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ import time
 from controller import Control
 
 import csv
-with open('eggs.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=';',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['|Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(["""Spam
-    Really Spam""", "Not|lovely", 'Wonderful Spam|'])
+addition = "какая-то строка\r\n"
+
+with open('eggs.csv', 'r+', newline='') as csvfile:
+    content = []
+    spamreader = csv.reader(csvfile, delimiter=';')
+    for row in spamreader:
+        content.append(row)
+    # content = csvfile.readlines()
+    print(content)
+    print(content[0][1])
+    # with open('notebook.csv', 'a', newline='') as csvfile1:
+    #     for n in content:
+    #         csvfile1.write(n)
+    #     csvfile1.write(addition)
+    #     csvfile1.writelines(content)
+
+with open('names.csv', 'w', newline='') as csvfile:
+    fieldnames = ['first_name', 'last_name']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
+
+    writer.writeheader()
+    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
 
 # Control.run('notebook.csv')
